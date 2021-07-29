152ba30e4ecb6e40feb84bbf4de482c5d0ef4b03
McJty <mcjty1@gmail.com>
2021-07-29 06:38:27 +0200

    Mod setup and first item

---
+++ src/main/java/com/mcjty/items/TestItem.java
@@ -0,0 +1,10 @@
+package com.mcjty.items;
+
+import net.minecraft.world.item.Item;
+
+public class TestItem extends Item {
+
+    public TestItem(Properties properties) {
+        super(properties);
+    }
+}
---
+++ src/main/java/com/mcjty/setup/Registration.java
@@ -0,0 +1,22 @@
+package com.mcjty.setup;
+
+import com.mcjty.items.TestItem;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.fmllegacy.RegistryObject;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+
+import static com.mcjty.tutorial.Tutorial.MODID;
+
+public class Registration {
+
+    private static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, MODID);
+
+    public static void init() {
+        ITEMS.register(FMLJavaModLoadingContext.get().getModEventBus());
+    }
+
+    public static final RegistryObject<Item> TESTITEM = ITEMS.register("testitem", () -> new TestItem(new Item.Properties()));
+
+}
